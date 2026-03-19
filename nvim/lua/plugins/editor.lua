b81087394d5d371a2d53391ddfaa3236fcf58551
Matthew Callens <callensmatt@gmail.com>
2026-03-19 12:23:51 -0400

    update tool configs

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -16,7 +16,36 @@ return {
     "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local null_ls = require "null-ls"
+      local h = require "null-ls.helpers"
+
+      local oxfmt = h.make_builtin {
+        name = "oxfmt",
+        meta = { url = "https://github.com/aspect-build/rules_lint" },
+        method = null_ls.methods.FORMATTING,
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "css",
+          "html",
+        },
+        generator_opts = {
+          command = "oxfmt",
+          args = { "--stdin-filepath", "$FILENAME" },
+          to_stdin = true,
+        },
+        factory = h.formatter_factory,
+      }
+
       opts.sources = require("astrocore").list_insert_unique(opts.sources, {
+        oxfmt.with {
+          condition = function(utils)
+            return utils.root_has_file {
+              ".oxfmtrc.json",
+            }
+          end,
+        },
         null_ls.builtins.formatting.prettier.with {
           condition = function(utils)
             return utils.root_has_file {
